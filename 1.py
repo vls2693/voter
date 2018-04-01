0797ff30330191e141703853d649996e6541f960
vl2693 <belyakov38@gmail.com>
2018-04-01 16:43:14 +0300

    added comments for all steps

--- 1.py
+++ 1.py
@@ -1,16 +1,16 @@
 import time
 from selenium import webdriver
 
-driver = webdriver.Firefox()
-driver.get("https://www.kinopoisk.ru/")
-time.sleep(1)
-driver.find_element_by_class_name("header-fresh-user-partial-component__login-button").click()
-time.sleep(1)
-frame = driver.find_element_by_class_name('kp2-authapi-iframe')
-driver.switch_to.frame(frame)
-auth1 = driver.find_element_by_name('login').send_keys('ololo@mail.ru')
-auth2 = driver.find_element_by_name('password').send_keys('ololo@mail.ru')
-time.sleep(3)
-driver.find_element_by_class_name('button__label').click()
-time.sleep(2)
-#driver.quit()
+driver = webdriver.Firefox() #открываем браузер
+driver.get("https://www.kinopoisk.ru/") #заходим на кинопоиск
+time.sleep(1) #ждём 1 секунду
+driver.find_element_by_class_name("header-fresh-user-partial-component__login-button").click() #нажимаем кнопку войти
+time.sleep(1) #ждём 1 секунду
+frame = driver.find_element_by_class_name('kp2-authapi-iframe') #находим окно регистрации/аутентификации
+driver.switch_to.frame(frame) #заходим в окно регистрации/аутентификации
+auth1 = driver.find_element_by_name('login').send_keys('ololo@mail.ru') #вводим логин/почту
+auth2 = driver.find_element_by_name('password').send_keys('ololo@mail.ru') #вводим пароль
+time.sleep(3) #ждём 3 секунды
+driver.find_element_by_class_name('button__label').click() #нажимаем кнопку войти
+time.sleep(2) #ждём 2 секунды
+#driver.quit() #завершаем работу
